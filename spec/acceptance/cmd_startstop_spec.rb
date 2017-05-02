336db046f1b730daf9e9728be692dd95e393a20a
Jordi Prats <jprats@systemadmin.es>
2017-05-02 13:49:34 +0200

    deshabilitatr service

--- spec/acceptance/cmd_startstop_spec.rb
+++ spec/acceptance/cmd_startstop_spec.rb
@@ -15,18 +15,15 @@ describe 'initscript context' do
         cmd_stop  => 'pkill sleep',
       }
 
-      ->
-
-      service { 'democmdstart':
-        ensure => 'running',
-        enable => true,
-      }
-
       EOF
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    it "start service" do
+     expect(shell("/etc/init.d/democmdstart start").exit_code).to be_zero
     end
 
     it "sleep started" do
@@ -41,18 +38,15 @@ describe 'initscript context' do
         cmd_stop  => 'pkill sleep',
       }
 
-      ->
-
-      service { 'democmdstart':
-      ensure => 'stopped',
-        enable => false,
-      }
-
       EOF
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    it "stop service" do
+     expect(shell("/etc/init.d/democmdstart stop").exit_code).to be_zero
     end
 
     it "sleep stopped" do
